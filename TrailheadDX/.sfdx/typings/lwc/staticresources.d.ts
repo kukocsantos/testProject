54477af57e1e16e15d1d0cc63ed92119bab4b81c
leekukocsantos <kukoc.santos@gmail.com>
2019-07-26 11:55:57 +0800

    Push to production

---
+++ TrailheadDX/.sfdx/typings/lwc/staticresources.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/resourceUrl/SiteSamples" {
+    var SiteSamples: string;
+    export default SiteSamples;
+}
+declare module "@salesforce/resourceUrl/vfimagetest" {
+    var vfimagetest: string;
+    export default vfimagetest;
+}
